4ff0e1e76a15416bd9cffb52813b1e2ba9708ffc
ductnf8 <ductnf8@fullstack.edu.vn>
2025-08-29 17:58:45 +0700

    n

--- ECOMMER_APP/frontend/src/App.jsx
+++ ECOMMER_APP/frontend/src/App.jsx
@@ -10,22 +10,28 @@ import Login from "./pages/Login.jsx";
 import PlaceOrder from "./pages/PlaceOrder.jsx";
 import Orders from "./pages/Orders.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import SearchBar from "./components/SearchBar.jsx";
+import {ToastContainer} from "react-toastify";
 
 const App = () => {
     return (
         <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+            <ToastContainer/>
             <Navbar/>
+            <SearchBar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/collection" element={<Collection/>}/>
                 <Route path="/about" element={<About/>}/>
                 <Route path="/contact" element={<Contact/>}/>
-                <Route path="/product" element={<Product/>}/>
+                <Route path="/product/:productId" element={<Product/>}/>
                 <Route path="/cart" element={<Cart/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/place-order" element={<PlaceOrder/>}/>
                 <Route path="/orders" element={<Orders/>}/>
             </Routes>
+            <Footer/>
         </div>
     )
 }
--- ECOMMER_APP/frontend/src/components/Navbar.jsx
+++ ECOMMER_APP/frontend/src/components/Navbar.jsx
@@ -1,14 +1,19 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {assets} from "../../assets/frontend_assets/assets.js";
 import {Link, NavLink} from "react-router-dom";
+import shopContext, {ShopContext} from "../context/ShopContext.jsx";
 
 const Navbar = () => {
 
     const [visible, setVisible] = useState(false);
 
+    const {setShowSearch, getCartCount} = useContext(ShopContext)
+
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
-            <img src={assets.logo} className='w-36' alt=""/>
+            <Link to='/'>
+                <img src={assets.logo} className='w-36' alt=""/>
+            </Link>
 
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
                 <NavLink to='/' className='flex flex-col items-center gap-1'>
@@ -30,7 +35,8 @@ const Navbar = () => {
             </ul>
 
             <div className='flex items-center gap-6'>
-                <img src={assets.search_icon} className='w-5 cursor-pointer' alt=""/>
+                <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer'
+                     alt=""/>
                 <div className='group relative'>
                     <img src={assets.profile_icon} className='w-5 cursor-pointer' alt=""/>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
@@ -43,7 +49,11 @@ const Navbar = () => {
                 </div>
                 <Link to='/cart' className='relative'>
                     <img src={assets.cart_icon} className='w-5 min-w-5' alt=""/>
-                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>
+                        {
+                            getCartCount()
+                        }
+                    </p>
                 </Link>
                 <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden'
                      alt=""/>
